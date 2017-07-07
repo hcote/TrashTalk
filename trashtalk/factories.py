a0acb4811cd4dc623b4cbd73e6f51ce786466f99
TangoYankee <miller.tim108@gmail.com>
2017-07-07 16:17:52 -0700

    Update Cleanups
    Commented out the variables that do not have a counterpart in the model
    Added in variables that are in the model but were not in the factory

--- trashtalk/factories.py
+++ trashtalk/factories.py
@@ -27,16 +27,17 @@ def cleanup_factory(data):
         date=data.get('date'),
         start_time=data.get('start_time'),
         end_time=data.get('end_time'),
-        street_name=data.get('street_name'),
-        street_number=data.get('street_number'),
-        cross_street_name=data.get('cross_street_name'),
+        #street_name=data.get('street_name'),
+        #street_number=data.get('street_number'),
+        #cross_street_name=data.get('cross_street_name'),
         image=data.get('image'),
-        city=data.get('city'),
-        state=data.get('state'),
-        zipcode=data.get('zipcode'),
-        county=data.get('county'),
-        district=data.get('district'),
-        country=data.get('country'),
+        host=data.get('host')#, added variable
+        #city=data.get('city'),
+        #state=data.get('state'),
+        #zipcode=data.get('zipcode'),
+        #county=data.get('county'),
+        #district=data.get('district'),
+        #country=data.get('country'),
     )
     cleanup.save()
     return cleanup
