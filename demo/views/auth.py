976bd239779ee98c05b6e7351fc8fbe124f8af9e
TangoYankee <miller.tim108@gmail.com>
2017-08-23 20:22:39 -0700

    Update auth.py

--- demo/views/auth.py
+++ demo/views/auth.py
@@ -1,13 +1,14 @@
-from flask import Blueprint, render_template, request, make_response, redirect, url_for
+from flask import Blueprint, render_template, request, make_response, redirect, url_for,flash
 from flask_login import login_user, login_required
 from flask_login import logout_user, current_user
 
 from trashtalk import app, login_manager
 from trashtalk.models import User, db_session
 from trashtalk.utils import status
+from trashtalk.html_constants import HtmlConstants
 
 auth = Blueprint('auth', __name__, template_folder='templates')
-
+html_constants=HtmlConstants()
 
 @login_manager.unauthorized_handler
 def unauthorized_handler():
@@ -43,9 +44,11 @@ def login():
             return redirect(url_for("cleanups.get_all"))
         else:
             # Next step: require user feedback password or username incorrect
+            flash("Incorrect Username or Password")
             return redirect(url_for("welcome"), code=200)
     else:
         # Next Step: require user feedback password or username incorrect
+        flash("Incorrect Username or Password")
         return redirect(url_for("welcome"), code=400)
 
 
@@ -59,8 +62,6 @@ def logout():
     user.save()
     logout_user()  # Flask Update
 
-    # Next Step: May be better to return to homepage
-    # return render_template("logout.html", section="Log out")
     return redirect(url_for("welcome"), code=200)
 
 
@@ -69,20 +70,28 @@ def create_account():
     # TODO: fields for optional inputs: i.e email and volunteer hours
     # Pull data from html form
     new_name = request.form["username"]
-    new_password = request.form["password"]
-    new_confirm_password = request.form["confirm_password"]
     # TODO: Add validator to table to avoid registering unavailable names
-    if new_password == new_confirm_password:
-        # Create account and log in user
-        new_user = User(username=new_name, password=new_password)
-        new_user.hash_password()  # Encrypt password
-        new_user.authenticated = True  # Login user to SQL
-        db_session.add(new_user)
-        db_session.commit()
-        login_user(new_user, remember=True)  # Login user to Flask
-        # return redirect(url_for('users.get', user_id=new_user.id), code=status.HTTP_201_CREATED)  # Send to profile page
-    #     temporarily changed to all cleanups, until profile page actually does something
-        return redirect(url_for('cleanups.get_all'))
-    else:
-        # Next Step: feedback that passwords do not match
+    # Check Username exists
+    if db_session.query(User).filter(User.username == new_name).count():
+        #Send back to try again
+        # print("%s is taken" % new_name)
+        flash("username: %s, is taken" % new_name)
         return redirect(url_for("signup"), code=status.HTTP_403_FORBIDDEN)
+    else:
+        #enter password validation process
+        new_password = request.form["password"]
+        new_confirm_password = request.form["confirm_password"]
+        if new_password == new_confirm_password:
+            # Create account and log in user
+            new_user = User(username=new_name, password=new_password, email=request.form['email'])
+            new_user.hash_password()  # Encrypt password
+            new_user.authenticated = True  # Login user to SQL
+            db_session.add(new_user)
+            db_session.commit()
+            login_user(new_user, remember=True)  # Login user to Flask
+            app.logger.info("%s successfully created an account" % new_name)
+            return redirect(url_for('cleanups.get_all'))
+        else:
+            # Next Step: feedback that passwords do not match
+            flash("passwords do not match")
+            return redirect(url_for("signup"), code=status.HTTP_403_FORBIDDEN)
