454f7e217d297cd7e985a6e8a7823672620d763c
TangoYankee <miller.tim108@gmail.com>
2017-09-05 12:08:04 -0700

    remove demo

--- demo/run.py
+++
@@ -1,8 +0,0 @@
-from trashtalk import app
-
-if __name__ == '__main__':
-    # Uncomment to override common configuration. config_file is a path to config set in
-    #  env var
-    # app.config.from_envvar(config_file)
-    app.run()
-
