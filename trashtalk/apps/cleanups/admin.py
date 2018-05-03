1eafce9e5c540239e14927031a37242d6da673a3
Thinh Ngo <thinh.ngo@gmail.com>
2018-05-03 10:11:54 -0700

    #171 added admin section for tools/tool categories

--- trashtalk/apps/cleanups/admin.py
+++ trashtalk/apps/cleanups/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-from .models import Cleanup, Location
+from .models import Cleanup, Location, RequiredTool, Tool, ToolCategory
+
+# pylint: disable=missing-docstring
+class ToolAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    list_filter = ['category']
+
 
 # Register your models here.
 admin.site.register(Cleanup)
 admin.site.register(Location)
+admin.site.register(RequiredTool)
+admin.site.register(Tool, ToolAdmin)
+admin.site.register(ToolCategory)
